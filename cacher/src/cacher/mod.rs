cf2d3a9a3d6ef460d98c6f81a558d886148abe7d
WarSame <graemecliffe@gmail.com>
2019-02-26 23:10:01 -0500

    Working cacher

--- cacher/src/cacher/mod.rs
+++ cacher/src/cacher/mod.rs
@@ -1,12 +1,13 @@
 use std::collections::hash_map;
 use std::hash::Hash;
+use std::rc::Rc;
 
 pub struct Cacher<T, K, V> where T: Fn(K) -> V {
     calculation: T,
-    values: hash_map::HashMap<K, V>
+    values: hash_map::HashMap<K, Rc<V>>
 }
 
-impl<T, K: Eq + Hash + Clone, V: Clone> Cacher<T, K, V>
+impl<T, K: Eq + Hash + Clone, V> Cacher<T, K, V>
     where T: Fn(K) -> V
 {
     pub fn new(calculation: T) -> Cacher<T, K, V>{
@@ -16,11 +17,10 @@ impl<T, K: Eq + Hash + Clone, V: Clone> Cacher<T, K, V>
         }
     }
 
-    pub fn value(&mut self, k: K) -> &V {
-        if self.values.contains_key(&k){
-            return &self.values[&k]
-        }
-        self.values.insert(k.clone(), (self.calculation)(k.clone()));
-        self.values.get(&k).unwrap()
+    pub fn value(&mut self, k: K) -> Rc<V> {
+        let f_ref = &self.calculation;
+        self.values.entry(k.clone())
+            .or_insert_with(|| Rc::new( f_ref(k) ))
+            .clone()
     }
 }
--- cacher/src/lib.rs
+++ cacher/src/lib.rs
@@ -7,8 +7,8 @@ mod tests {
     #[test]
     fn repeated_runs_same() {
         let mut cacher = Cacher::new(|x| x);
-        let run1 = cacher.value(5) as *const i32;
-        let run2 = cacher.value(7) as *const i32;
+        let run1 = cacher.value(5);
+        let run2 = cacher.value(7);
 
         assert_ne!(run1, run2);
     }
